00ad324c2575137adf979f53e917899f2cd8a2b2
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-05-30 01:09:48 -0300

    Inicio dos trabalhos

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export default function App() {
+  return(
+    <Corpo>
+        aoba
+    </Corpo>
+  );
+}
+const Corpo = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Lexend Deca', 'sans-serif';
+`
