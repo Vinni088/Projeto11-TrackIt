7f27c840e82c9fb8a9d08519147ae84f23515cfd
Vinicius Evangelista <viniciusevangelista088@gmail.comgit config --global user.name Vinni>
2023-06-05 13:52:24 -0300

    Funcional + data-test3

--- src/Paginas/TelaHoje.jsx
+++ src/Paginas/TelaHoje.jsx
@@ -6,7 +6,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { UserContext } from "/src/App.jsx";
 import { useContext, useEffect, useState } from "react";
-
+import { CircularProgressbar } from "react-circular-progressbar";
 
 
 
@@ -20,6 +20,7 @@ export default function TelaHoje() {
             Authorization: `Bearer ${context.token}`
         }
     }
+    let n_porcent = 0;
 
     useEffect(() => {
         const requisicao = axios.get(url_habitos_hoje, chave);
@@ -53,28 +54,51 @@ export default function TelaHoje() {
         if(!cond){
             let url_positiva = `https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${x}/check`;
             let promisse = axios.post(url_positiva, {} ,chave);
-            SetAtualize(atualize+1);
+            setTimeout(SetAtualize, 1500, atualize+1);
         } else {
             let url_negativa = `https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/${x}/uncheck`;
             let promisse = axios.post(url_negativa, {} ,chave);
-            SetAtualize(atualize+1);
+            setTimeout(SetAtualize, 1500, atualize+1);
         }
+    }
+    function PorcentagemHoje(){
+        let n_hoje = habitos.length;
+        let n_feitos = 0;
+        for (let i = 0; i < habitos.length; i++) {
+            if(habitos[i].done) {
+                n_feitos++;
+            }
+        }
+        n_porcent = n_feitos / n_hoje ;
+        if(n_porcent > 0){
+            return(
+                `${n_porcent*100}% dos hábitos concluídos`
+            );
+        } else {
+            return(
+                `Nenhum hábito concluído ainda`
+            );
+        }
+        
     }
 
     if(habitos !== null) {return(
         <HojeBody>
-            <HojeData>
+            <HojeTopo>
                 <h1 data-test="today">
                     {weekday(dayjs().day())}, {dayjs().format(' MM-DD ') /*THH:mm:ss SSS [Z] A */}
                 </h1>
-            </HojeData>
+                <h2 pct={n_porcent} data-test="today-counter">
+                    {`${PorcentagemHoje()}`}
+                </h2>
+            </HojeTopo>
             <HabitoContainer>
             {habitos.map((habito,indice) =>{
                 return (
                 <Habito data-test="today-habit-container" key={indice} done={habito.done}>
-                    <h1 data-test="today-habit-name">{habito.name}</h1>
-                    <h2 data-test="today-habit-sequence">Sequencia Atual: {habito.currentSequence}</h2>
-                    <h3 data-test="today-habit-record">Seu recorde: {habito.highestSequence}</h3>
+                    <Habito_h1 data-test="today-habit-name">{habito.name}</Habito_h1>
+                    <Habito_h2 seqDone={habito.done} data-test="today-habit-sequence">Sequencia Atual: {habito.currentSequence}</Habito_h2>
+                    <Habito_h3 seq1={habito.currentSequence} seq2={habito.highestSequence} data-test="today-habit-record">Seu recorde: {habito.highestSequence}</Habito_h3>
                    <img data-test="today-habit-check-btn" onClick={() => checkmark(habito.done, habito.id)} src={check} alt="Check Habito" />
                 </Habito>)
             })}
@@ -112,9 +136,17 @@ const HojeBody = styled.div`
     padding-left: 15px;
     padding-right: 15px;
 `
-const HojeData = styled.div`
+const HojeTopo = styled.div`
+
     margin-top: 95px;
     margin-bottom: 15px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 5px;
+
     h1 {
         width: 200px;
         height: 30px;
@@ -125,6 +157,16 @@ const HojeData = styled.div`
         font-size: 22.976px;
         line-height: 29px;
         color: #126BA5;
+    }
+    h2 {
+
+        font-family: 'Lexend Deca';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 17.976px;
+        line-height: 22px;
+
+        color: ${props => props.pct > 0 ? '#8FC549': '#BABABA'} ;
     }
 `
 const Menu = styled.div`
@@ -168,40 +210,6 @@ const Habito = styled.div`
     background: #FFFFFF;
     border-radius: 5px;
 
-    h1{
-        left: 15px;
-        top: 13px;
-        position: absolute;
-
-        font-family: 'Lexend Deca';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 19.976px;
-        line-height: 25px;
-        color: #666666;
-    }
-    h2 {
-        left: 15px;
-        top: 36px;
-        position: absolute;
-
-        font-family: 'Lexend Deca';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 12.976px;
-        line-height: 16px;
-    }
-    h3 {
-        left: 15px;
-        top: 59px;
-        position: absolute;
-
-        font-family: 'Lexend Deca';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 12.976px;
-        line-height: 16px;
-    }
     img {
 
         box-sizing: border-box;
@@ -215,4 +223,40 @@ const Habito = styled.div`
         border: 1px solid #E7E7E7;
         border-radius: 5px;
     }
+`
+const Habito_h1 = styled.h1`
+    left: 15px;
+    top: 13px;
+    position: absolute;
+
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19.976px;
+    line-height: 25px;
+    color: #666666;
+`
+const Habito_h2 = styled.h2`
+    left: 15px;
+    top: 40px;
+    position: absolute;
+
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12.976px;
+    line-height: 16px;
+    color: ${props => props.seqDone ? '#8FC549': 'black'} ;
+`
+const Habito_h3 = styled.h3`
+    left: 15px;
+    top: 59px;
+    position: absolute;
+
+    font-family: 'Lexend Deca';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12.976px;
+    line-height: 16px;
+    color: ${props => props.seq1 === props.seq2? '#8FC549': 'black'} ;
 `
